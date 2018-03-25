0a0b9d8ba7e9144500de12ca389d2b70f308983a
Khathawut Chaipraphun <bomb_khathawut@hotmail.com>
2018-03-25 23:32:41 +0700

    can GET array user

---
+++ index.js
@@ -0,0 +1,33 @@
+let app = require('express')();
+let users = require('./users');
+let bodyParser = require('body-parser');
+
+let port = process.env.PORT || 7777;
+
+// parse application/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', function (req, res) {
+    res.send('<h1>Hello Node.js</h1>');
+});
+
+app.get('/user', function (req, res) {
+    res.json(users.findAll());
+});
+
+app.get('/user/:id', function (req, res) {
+    var id = req.params.id;
+    res.json(users.findById(id));
+});
+
+app.post('/newuser', function (req, res) {
+    let json = req.body;
+    res.send('Add new ' + json.name + ' Completed!');
+});
+
+app.listen(port, function() {
+    console.log('Starting node.js on port ' + port);
+});
